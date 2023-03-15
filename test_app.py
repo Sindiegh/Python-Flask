fd87d7ecefa23dd858653c71f803e068c25b5072
Sindiegh <sindiegh.sk@gmail.com>
2023-03-15 11:07:23 +0200

    added styles and pytest

--- test_app.py
+++ test_app.py
@@ -1,1 +1,14 @@
 import pytest
+from flask.testing import FlaskClient
+from flask import Flask, url_for
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client: FlaskClient):
+    response = client.get('/')
+    assert response.status_code ==200
+    assert b"search" in response.data
